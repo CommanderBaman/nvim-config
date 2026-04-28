42e70cd37c61a298ef7c6cc33c6b00b057dd3c32
CommanderBaman <shubhamojha18@gmail.com>
2026-04-28 11:06:40 +0530

    update with kickstart repo

--- lua/commanderbaman/plugin/mini.lua
+++ lua/commanderbaman/plugin/mini.lua
@@ -1,13 +1,19 @@
 -- collection of various small independent plugins
 return {
-    'echasnovski/mini.nvim',
+    'nvim-mini/mini.nvim',
     config = function()
         -- Better Around/Inside textobjects
         -- Examples:
         --  - va)  - [V]isually select [A]round [)]paren
-        --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
+        --  - yiiq - [Y]ank [I]nside [N]ext [Q]uote
         --  - ci'  - [C]hange [I]nside [']quote
-        require('mini.ai').setup { n_lines = 500 }
+        require('mini.ai').setup {
+            mappings = {
+                around_next = 'aa',
+                inside_next = 'ii',
+            },
+            n_lines = 500,
+        }
 
         -- Add/delete/replace surroundings (brackets, quotes, etc.)
         -- Examples:
