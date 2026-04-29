368346d2739e8543b3f4a6bb91b1424fcf71a936
CommanderBaman <shubhamojha18@gmail.com>
2026-04-29 15:59:02 +0530

    show hidden files in neo tree

--- lua/commanderbaman/plugin/neo-tree.lua
+++ lua/commanderbaman/plugin/neo-tree.lua
@@ -29,6 +29,9 @@ return {
                     ['\\'] = 'close_window',
                 },
             },
+            filtered_items = {
+                visible = true,
+            },
         },
     },
 }
